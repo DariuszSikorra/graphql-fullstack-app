50d5c67724ef82291506f344bbb36a3accf4a5b5
DariuszSikorra <dariusz.sikorra@gmail.com>
2024-01-17 15:35:48 +0100

    app base

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import LoginForm from "./components/LoginForm";
+
+function App() {
+  return (
+    <>
+      <h1>Your App</h1>
+      <LoginForm />
+    </>
+  );
+}
+
+export default App;
